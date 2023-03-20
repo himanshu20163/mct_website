e3bc81aea733c9d3e9219455b9af8530b2dc6e4b
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-03-20 18:13:58 +0530

    Update User.jsx

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './users.css';
+import './user.css';
 
 const User = () => {
   const [genderResult, setGenderResult] = useState({ results: [] });
